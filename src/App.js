fedd21d4b119b0c5f92590c976400801ca67dcbc
IanTaylor <76837421+iTaylorCodes@users.noreply.github.com>
2021-10-08 01:43:33 -0700

    Set default game settings/ implemented cell flip functionality

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <Board ncols={3} nrows={3} chanceLightStartsOn={0.5} />
+      <Board ncols={3} nrows={3} chanceLightStartsOn={0.25} />
     </div>
   );
 }
--- src/Board.js
+++ src/Board.js
@@ -27,7 +27,7 @@ import "./Board.css";
  *
  **/
 
-function Board({ nrows, ncols, chanceLightStartsOn }) {
+function Board({ nrows = 5, ncols = 5, chanceLightStartsOn = 0.5 }) {
   const [board, setBoard] = useState(createBoard());
 
   /** create a board nrows high/ncols wide, each cell randomly lit or unlit */
@@ -44,7 +44,7 @@ function Board({ nrows, ncols, chanceLightStartsOn }) {
   }
 
   function hasWon() {
-    // TODO: check the board in state to determine whether the player has won.
+    return board.every((row) => row.every((cell) => !cell));
   }
 
   function flipCellsAround(coord) {
@@ -59,11 +59,15 @@ function Board({ nrows, ncols, chanceLightStartsOn }) {
         }
       };
 
-      // TODO: Make a (deep) copy of the oldBoard
+      const copy = oldBoard.map((row) => [...row]);
 
-      // TODO: in the copy, flip this cell and the cells around it
+      flipCell(y, x, copy);
+      flipCell(y, x - 1, copy);
+      flipCell(y, x + 1, copy);
+      flipCell(y - 1, x, copy);
+      flipCell(y + 1, x, copy);
 
-      // TODO: return the copy
+      return copy;
     });
   }
 
